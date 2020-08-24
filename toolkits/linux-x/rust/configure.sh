389c10918313f516fc71435a14aa46014c018528
Iron Oxidizer <ironoxidizer@gmail.com>
2020-08-24 16:46:52 -0400

    Updated druid to draw

--- toolkits/linux-x/rust/configure.sh
+++ toolkits/linux-x/rust/configure.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+sudo apt-get install -y gcc
+
 curl -sSf https://sh.rustup.rs/ | sh -s -- -y
 
 PATH="$PATH:$HOME/.cargo/bin"
--- toolkits/linux-x/rust/druid/src/main.rs
+++ toolkits/linux-x/rust/druid/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use druid::{AppLauncher, WindowDesc, Widget, PlatformError};
+use druid::widget::{Label, Flex, Padding, Align};
+
+fn build_ui() -> impl Widget<()> {
+    Padding::new(
+        10.0,
+        Flex::row()
+            .with_flex_child(
+                Flex::column()
+                    .with_flex_child(Label::new("top left"), 1.0)
+                    .with_flex_child(Align::centered(Label::new("bottom left")), 1.0),
+                1.0)
+            .with_flex_child(
+                Flex::column()
+                    .with_flex_child(Label::new("top right"), 1.0)
+                    .with_flex_child(Align::centered(Label::new("bottom right")), 1.0),
+                1.0)
+    )
+}
+
+fn main() -> Result<(), PlatformError> {
+    AppLauncher::with_window(WindowDesc::new(build_ui)).launch(())?;
+    Ok(())
 }
