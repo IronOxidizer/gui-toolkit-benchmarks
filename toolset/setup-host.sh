d3ad923b585df8d87a2f34811b46eb2c56ac9077
Iron Oxidizer <ironoxidizer@gmail.com>
2023-03-10 17:59:22 -0500

    Move setup-host to toolset

--- toolset/setup-host.sh
+++ toolset/setup-host.sh
@@ -16,7 +16,7 @@ mkdir -m 0755 -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
-apt-get install --no-install-recommends -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install --no-install-recommends -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin
 
 # Return to normal user
 su "$(logname)"
