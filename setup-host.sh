4288bfc2d11e9bb17b9077e5a2c94c24f3d2a164
Iron Oxidizer <60191958+IronOxidizer@users.noreply.github.com>
2023-03-10 14:32:04 -0500

    Update setup-host.sh

--- setup-host.sh
+++ setup-host.sh
@@ -9,14 +9,14 @@ fi
 apt-get update && apt-get upgrade
 
 # Install dependencies
-apt-get install --no-install-recommends ca-certificates curl gnupg lsb-release xorg openbox dbus-user-session
+apt-get install --no-install-recommends -y ca-certificates curl gnupg lsb-release xorg openbox dbus-user-session
 
 # Install docker
 mkdir -m 0755 -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
-apt-get install --no-install-recommends docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install --no-install-recommends -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Return to normal user
 su "$(logname)"
