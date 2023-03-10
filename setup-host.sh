9f6201695fc4a22f5553494111736337245011ac
Iron Oxidizer <60191958+IronOxidizer@users.noreply.github.com>
2023-03-10 13:00:21 -0500

    Create setup-host.sh

---
+++ setup-host.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ `whoami` != 'root' ];
+then
+    echo "This program needs to be run using 'sudo'"
+    exit
+fi
+
+apt-get update && apt-get upgrade
+
+# Install docker
+apt-get install ca-certificates curl gnupg lsb-release
+mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
