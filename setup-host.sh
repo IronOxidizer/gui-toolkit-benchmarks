13fa94f665178c75430951830ce0f1eb462fc62d
Iron Oxidizer <60191958+IronOxidizer@users.noreply.github.com>
2023-03-10 13:16:40 -0500

    Update setup-host.sh

--- setup-host.sh
+++ setup-host.sh
@@ -8,10 +8,19 @@ fi
 
 apt-get update && apt-get upgrade
 
+# Install dependencies
+apt-get install --no-install-recommends ca-certificates curl gnupg lsb-release xorg openbox 
+
 # Install docker
-apt-get install ca-certificates curl gnupg lsb-release
 mkdir -m 0755 -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install --no-install-recommends docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Return to normal user
+su "$(logname)"
+
+# Setup display
+echo 'exec openbox' > ~/.xinitrc
+startx
