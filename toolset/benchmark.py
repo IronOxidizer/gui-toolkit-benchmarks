c9efba0498d4853bd22da6376fbe0033702ae7d7
Iron Oxidizer <60191958+IronOxidizer@users.noreply.github.com>
2023-03-11 15:52:51 -0500

    Update benchmark.py

--- toolset/benchmark.py
+++ toolset/benchmark.py
@@ -1,5 +1,5 @@
 #!/bin/python3
 
-#docker build . -t bench:latest -f druid.dockerfile .
+#docker build -t bench:latest -f druid.dockerfile .
 #xhost +
 #docker run --rm -it --network host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix test:0.1
