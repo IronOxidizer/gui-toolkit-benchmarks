2e954c096644bfd2e31689c483412106944962e8
Iron Oxidizer <ironoxidizer@gmail.com>
2023-04-11 00:41:56 -0400

    First working version

--- toolset/pixelpeep.c
+++ toolset/pixelpeep.c
@@ -16,6 +16,13 @@
  * - 1.030ms C        XGetImage
  * - 0.204ms C        xcb_image_get
  * - 0.063ms C        xcb_shm_get_image_unchecked
+ * 
+ * Example python usage:
+ * 
+ *   from ctypes import cdll
+ *   pixelpeep = cdll.LoadLibrary("./pixelpeep.so")
+ *   result = pixelpeep.await_stable_image(b"app-name", 0, 0)
+ *   print("Result:", result)
  */
 
 #include <xcb/xcb.h>
@@ -24,10 +31,13 @@
 #include <sys/shm.h>
 
 #include <inttypes.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+// debug dependencies
+#include <stdio.h>
+#include <png.h> // add -lpng
 
 #define AllPlanes       ~0u // https://refspecs.linuxfoundation.org/LSB_5.0.0/LSB-Desktop-generic/LSB-Desktop-generic.html
 #define BYTES_PER_PIXEL 4   // assume 4 bytes per pixel
@@ -43,7 +53,6 @@ xcb_window_t search_window_by_name(xcb_connection_t *conn, xcb_window_t window, 
 
     // get children
     xcb_query_tree_reply_t *tree_reply = xcb_query_tree_reply(conn, xcb_query_tree(conn, window), NULL);
-    if (!tree_reply) return 0;
     xcb_window_t *children = xcb_query_tree_children(tree_reply);
     int num_children = xcb_query_tree_children_length(tree_reply);
     free(tree_reply);
@@ -56,14 +65,33 @@ xcb_window_t search_window_by_name(xcb_connection_t *conn, xcb_window_t window, 
     return 0;
 }
 
+// for debugging purposes only
+void write_image_to_file(unsigned char* frame_buffer, int width, int height, char* filename) {
+    FILE *fp = fopen(filename, "wb");
+    png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    png_infop info_ptr = png_create_info_struct(png_ptr);
+    setjmp(png_jmpbuf(png_ptr));
+    png_init_io(png_ptr, fp);
+    png_set_IHDR(png_ptr, info_ptr, width, height, 8, PNG_COLOR_TYPE_RGB_ALPHA, PNG_INTERLACE_NONE,
+                 PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+    png_write_info(png_ptr, info_ptr);
+    unsigned char **row_pointers = (unsigned char**) malloc(sizeof(unsigned char*) * height);
+    for (int y = 0; y < height; y++) row_pointers[y] = &frame_buffer[y * width * 4];
+    png_write_image(png_ptr, row_pointers);
+    png_write_end(png_ptr, NULL);
+    free(row_pointers);
+    fclose(fp);
+    png_destroy_write_struct(&png_ptr, &info_ptr);
+}
 
 /*
  * @param   char*   window_name the name of the window to monitor, if NULL, will monitor the entire screen
  * @param   uint    timeout     the max number of polls to do, where each poll takes ~1ms
  * @param   int     stabilizer  the min number of ticks required before the image is considered stable
  * @return  int                 time in milliseconds for the image to stabilize
- *                                  if the timeout was reached, this will return -1
- *                                  if the window could not be found, this will return -2
+ *                                  -1 if the timeout was reached
+ *                                  -2 if the window could not be found
+ *                                  -3 if the image never changed, this can happen if the window is not entirely in the screen
  */
 int await_stable_image(char *window_name, unsigned int timeout, unsigned int stabilizer) {
     // start timer
@@ -77,11 +105,28 @@ int await_stable_image(char *window_name, unsigned int timeout, unsigned int sta
     xcb_connection_t *conn = xcb_connect(NULL, NULL);
     xcb_screen_t *screen = xcb_setup_roots_iterator(xcb_get_setup(conn)).data;
     
-    // get window and properties
-    xcb_window_t window = window_name ? search_window_by_name(conn, screen->root, window_name) : screen->root;
-    if (!window) return 1;
+    // get window
+    xcb_window_t window;
+    if (window_name) {
+        window = search_window_by_name(conn, screen->root, window_name);
+        if (!window) return -2;
+        xcb_query_tree_reply_t *tree_reply = xcb_query_tree_reply(conn, xcb_query_tree(conn, window), NULL);
+        xcb_window_t parent = tree_reply->parent;
+        free(tree_reply);
+        while (parent != screen->root) {
+            window = parent;
+            tree_reply = xcb_query_tree_reply(conn, xcb_query_tree(conn, window), NULL);
+            parent = tree_reply->parent;
+            free(tree_reply);
+        }
+    } else {
+        window = screen->root;
+    }
+    
+    // get window geometry
     xcb_get_geometry_reply_t *geometry_reply = xcb_get_geometry_reply(conn, xcb_get_geometry(conn, window), NULL);
     uint16_t w = geometry_reply->width, h = geometry_reply->height;
+    int16_t x = geometry_reply->x, y = geometry_reply->y;
     free(geometry_reply);
     
     // setup shared memory
@@ -99,16 +144,17 @@ int await_stable_image(char *window_name, unsigned int timeout, unsigned int sta
         
         // update shared buffer
         xcb_shm_get_image_reply(conn, xcb_shm_get_image_unchecked(
-            conn, screen->root, 0, 0, w, h, AllPlanes, XCB_IMAGE_FORMAT_Z_PIXMAP, seg, 0), NULL);
+            conn, screen->root, x, y, w, h, AllPlanes, XCB_IMAGE_FORMAT_Z_PIXMAP, seg, 0), NULL);
         
         // reset if doesn't match
         if (memcmp(image_copy, shared_buffer, data_bytes)) {
             clock_gettime(1, &end_time);
-            fprintf(stderr, "Change detected");
             stable_ticks = 0;
             memcpy(image_copy, shared_buffer, data_bytes);
         }
     }
+    
+    //write_image_to_file((unsigned char *)image_copy, w, h, "test-image.png");
     
     // cleanup
     free(image_copy);
@@ -119,5 +165,6 @@ int await_stable_image(char *window_name, unsigned int timeout, unsigned int sta
     // return results
     if (!timeout) return -1;
     int elapsed_time = (int)((end_time.tv_sec - start_time.tv_sec) * 1000 + (end_time.tv_nsec - start_time.tv_nsec) / 1e6);
+    if (elapsed_time < 0) return -3;
     return elapsed_time;
 }
